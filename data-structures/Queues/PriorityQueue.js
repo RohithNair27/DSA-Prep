45d7880c5e0581cffe8aeabfbf8f4a765f164792
rohith <rohithnair2711@gmail.com>
2025-04-27 15:20:13 +0530

    feat:add priorityQueue -dequeue

--- data-structures/Queues/PriorityQueue.js
+++ data-structures/Queues/PriorityQueue.js
@@ -31,4 +31,47 @@ class PriorityQueue {
       return this.list;
     }
   }
+  dequeue() {
+    if (this.list.length === 0) return undefined;
+    let dequeueNode;
+
+    dequeueNode = this.list[0];
+    if (this.list.length === 1) {
+      this.list.pop();
+      return dequeueNode;
+    }
+    // Assigning the first element as the last element
+    // also removing the last element
+    this.list[0] = this.list.pop();
+    let currentIndex = 0;
+    // finding the both child in the array
+
+    let leftNode, rightNode;
+    while (true) {
+      let leftNodeIndex = 2 * currentIndex + 1;
+      let rightNodeIndex = 2 * currentIndex + 2;
+      let swap = null;
+      if (leftNodeIndex < this.list.length) {
+        leftNode = this.list[leftNodeIndex];
+        if (leftNode.priority < this.list[currentIndex].priority) {
+          swap = leftNodeIndex;
+        }
+      }
+      if (rightNodeIndex < this.list.length) {
+        rightNode = this.list[rightNodeIndex];
+        if (
+          (!swap && rightNode.priority < this.list[currentIndex].priority) ||
+          (swap && rightNode.priority < leftNode.priority)
+        ) {
+          swap = rightNodeIndex;
+        }
+      }
+      if (!swap) break; // break if no swap happen with children nodes
+      let temp = this.list[swap];
+      this.list[swap] = this.list[currentIndex];
+      this.list[currentIndex] = temp;
+      currentIndex = swap;
+    }
+    return dequeueNode;
+  }
 }
