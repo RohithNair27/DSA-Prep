2beeb5578c81069634d10cbda682417d292238d7
rohith <rohithnair2711@gmail.com>
2025-04-30 19:11:41 +0530

    Fix:add noSwap to improve performace

--- algorithms/BubbleSort.js
+++ algorithms/BubbleSort.js
@@ -1,12 +1,19 @@
+// o(n**2)
 function bubbleSort(arr) {
+  let noSwap = true;
   for (let i = arr.length - 1; i >= 0; i--) {
     for (let j = 0; j < i; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        noSwap = false; // If all the elements are already sorted
       }
     }
+    if (noSwap) break; // Improves performace
   }
   return arr;
 }
+
+console.log(bubbleSort([1, 2, -10, 10, 100, -99, 99, 200, -200]));
+console.log(bubbleSort([1, 2, 3, 200]));
